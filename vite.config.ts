04927cb595362d817d822322f7557fdfe4e0315b
Felix Schnabel <f.schnabel@tum.de>
2026-01-13 20:26:49 +0100

    Add basepath

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/DBD-HardcoreKiller-BloodMoney/',
   plugins: [
     react({
       babel: {
